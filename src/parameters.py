56fcdd84fc7175da63b538d55b6e7d808cd5d5fe
fperakis <f.perakis@fysik.su.se>
2017-12-20 11:49:20 +0100

    added and updated examples and src files

--- src/parameters.py
+++ src/parameters.py
@@ -15,3 +15,36 @@ def iceIh_parameters():
     dz = 6*dOOa
 
     return file_name, density, dx, dy, dz
+
+def iceXII_parameters():
+    # taken from Nature 391, 268 (1998) https://www.nature.com/articles/34622
+    file_name = 'ice12'
+    density_XII = 1.4665
+    density = density_XII*10**(-24)/((2*1.0079+15.999)/(6.022*10**23))
+
+    dOOa = 8.304
+    dOOc = 4.024
+
+    dx = dOOa
+    dy = dOOa
+    dz = 2*dOOc
+
+    return file_name, density, dx, dy, dz
+
+def iceIV_parameters():
+    # taken from http://www1.lsbu.ac.uk/water/ice_iv.html
+    file_name = 'ice4'
+    density_IV = 1.272
+
+    density = density_IV*10**(-24)/((2*1.0079+15.999)/(6.022*10**23))
+
+    dOOa = 7.60
+    angle = 70.1
+    dx = 6.9122#dOOa*np.sin(angle * np.pi / 180.)
+    dy = 1.8569#dOOa*np.cos(angle * np.pi / 180.)
+    #dx = 2*dOOa*np.cos((90-angle) * np.pi / 180.)
+    #dy = 2*dOOa*np.sin(angle * np.pi / 180.)
+    dz = 2*dOOa
+
+    return file_name, density, dx, dy, dz
+
